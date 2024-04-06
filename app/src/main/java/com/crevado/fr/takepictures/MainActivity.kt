1266df7860b7607bb7a4cd4a766711b7d86044a8
fazle <fazlerabbicse@gmail.com>
2024-04-06 16:30:57 +0600

    feat: optimize imports

--- app/src/main/java/com/crevado/fr/takepictures/MainActivity.kt
+++ app/src/main/java/com/crevado/fr/takepictures/MainActivity.kt
@@ -6,17 +6,9 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.crevado.fr.takepictures.ui.theme.TakePicturesTheme
 
 class MainActivity : ComponentActivity() {
 
