e0d8eee4e622edba3d73cd6d2c0fa0027ca9cb5c
fazle <fazlerabbicse@gmail.com>
2024-04-06 15:54:54 +0600

    feat: call the requestCameraPermission function on the onCreate method

--- app/src/main/java/com/crevado/fr/takepictures/MainActivity.kt
+++ app/src/main/java/com/crevado/fr/takepictures/MainActivity.kt
@@ -34,16 +34,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TakePicturesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Text(text = "Test")
         }
+
+        requestCameraPermission()
     }
 
     private fun requestCameraPermission() {
@@ -64,19 +58,5 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+}
 
-    @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        TakePicturesTheme {
-            Greeting("Android")
-        }
-    }
