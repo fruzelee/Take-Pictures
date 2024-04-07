61af01d367180525da1093b0002af1dd21dd6fe8
fazle <fazlerabbicse@gmail.com>
2024-04-07 16:22:15 +0600

    feat: keeping the output directory, camera executor, and should show camera boolean in memory. The functions help manage those properties.

--- app/src/main/java/com/crevado/fr/takepictures/MainActivity.kt
+++ app/src/main/java/com/crevado/fr/takepictures/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.crevado.fr.takepictures
 
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.Text
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import java.io.File
+import java.util.concurrent.ExecutorService
 
 class MainActivity : ComponentActivity() {
 
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
+    private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
 
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
@@ -50,5 +59,21 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-}
+    private fun handleImageCapture(uri: Uri) {
+        Log.i("test", "Image Captured: $uri")
+        shouldShowCamera.value = false
+    }
 
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+
+        return if (mediaDir != null && mediaDir.exists()) mediaDir else filesDir
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+}
